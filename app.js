347db4a72165f859149059ecd639a0850c07c2ee
Corey Benedict-Osborn <corey.benedict@gmail.com>
2022-12-31 17:50:01 -0500

    5-NodeJS: Week 3 - Express Session Part 2

--- app.js
+++ app.js
@@ -45,32 +45,18 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error(`You are not authenticated!`);
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
+        const err = new Error(`You are not authenticated!`);
+        err.status = 401;
         return next(err);
-        }
-
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === 'admin' && pass === 'password') {
-            req.session.user = 'admin';
-            return next();  // Authorized
-        } else {
-            const err = new Error(`You are not authenticated!`);
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
     } else {
-        if (req.session.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             return next();
         } else {
             const err = new Error(`You are not authenticated!`);
@@ -84,26 +70,25 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
